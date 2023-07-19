ff5030f273e55dec619377cfa9c28d21833864c9
JulKuzmi <enchantress000@mail.ru>
2023-07-19 20:29:48 +0300

    add third test

--- game.test.ts
+++ game.test.ts
@@ -4,3 +4,16 @@ import { cards } from './src/cards';
 it('should length of array', () => {
     expect(cards).toHaveLength(36);
 });
+
+it('should there be such a card', () => {
+    const numberCard = ['6 туз'];
+    expect(cards).not.toBe(numberCard);
+});
+
+it('should check that the contents of the cards are correct', () => {
+    const card: string[] = ['туз пики'];
+
+    expect(cards).toContain('туз пики');
+    expect(new Set(cards)).toContain('туз пики');
+    expect(card).toContain('туз пики');
+});
