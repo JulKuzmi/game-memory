85df98e3fb0c586e1d8188fb7c1c59f76693009c
JulKuzmi <enchantress000@mail.ru>
2023-07-23 17:43:26 +0300

    fixed logic game

--- README.md
+++ README.md
@@ -1,10 +1,11 @@
 # game-memory
 
-project description: a card game in which you have to solve all pairs of cards
+Описание: в проекте реализована карточная игра, суть которой заключается в том, что Вам нужно разгадать все пары карт.
 
 В проекте используется следующий стек технологий и инструментов:
 -TypeScript
 -Webpack
 -Jest
+
 Для запуска приложения используйте следующую команду:
 npm run start
--- src/game.ts
+++ src/game.ts
@@ -3,8 +3,8 @@ import './css/page.css';
 import { openPage } from './index';
 export function gameLogic(add: HTMLElement, cardsCount: number) {
     let begin = '';
-    let firstCard: string | undefined;
-    let secondCard: string | undefined;
+    // let firstCard: string | undefined;
+    // let secondCard: string | undefined;
     let hourseTime = '';
     let random: string[] = [];
     for (let i = 0; i < cardsCount / 2; i++) {
@@ -74,6 +74,9 @@ export function gameLogic(add: HTMLElement, cardsCount: number) {
     const card: HTMLInputElement[] = Array.from(
         document.querySelectorAll('.card'),
     );
+
+    let count = 0;
+    let firstCard: string | undefined;
     card.forEach((car) => {
         car.addEventListener('click', () => {
             car.setAttribute(
@@ -83,9 +86,33 @@ export function gameLogic(add: HTMLElement, cardsCount: number) {
             if (!firstCard) {
                 firstCard = car.dataset.value;
             } else {
-                stopTimer();
-                secondCard = car.dataset.value;
-                if (firstCard !== secondCard) {
+                let secondCard = car.dataset.value;
+                count += 1;
+                if (firstCard.charAt(0) === secondCard?.charAt(0)) {
+                    firstCard = '';
+                    secondCard = car.dataset.value;
+                    count += 1;
+                    if (cardsCount === count) {
+                        stopTimer();
+                        const winner = `<div class="container-end">
+                    <div class="text">
+                    <div><img class="image"src="./static/asset/jpg/winner.png" alt="winner"></div>
+                    <h1 class="title-end">Вы выиграли!</h1>
+                    </div>
+                    <div class="text-end">Затраченное время:</div>
+                    <div class="time-end">${hourseTime}</div>
+                    <button class="startagain">Играть снова</button>
+                    </div>`;
+                        add.innerHTML = winner;
+                        const buttonAgain = document.querySelector(
+                            '.startagain',
+                        ) as HTMLElement;
+                        buttonAgain.addEventListener('click', () => {
+                            return openPage();
+                        });
+                    }
+                } else {
+                    stopTimer();
                     const looser = `<div class="container-end">
                     <div class="text">
                     <div><img class="image"src="./static/asset/jpg/looser.png" alt="looser"></div>
@@ -96,23 +123,6 @@ export function gameLogic(add: HTMLElement, cardsCount: number) {
                     <button class="startagain">Играть снова</button>
                     </div>`;
                     add.innerHTML = looser;
-                    const buttonAgain = document.querySelector(
-                        '.startagain',
-                    ) as HTMLElement;
-                    buttonAgain.addEventListener('click', () => {
-                        return openPage();
-                    });
-                } else {
-                    const winner = `<div class="container-end">
-                    <div class="text">
-                    <div><img class="image"src="./static/asset/jpg/winner.png" alt="winner"></div>
-                    <h1 class="title-end">Вы выиграли!</h1>
-                    </div>
-                    <div class="text-end">Затраченное время:</div>
-                    <div class="time-end">${hourseTime}</div>
-                    <button class="startagain">Играть снова</button>
-                    </div>`;
-                    add.innerHTML = winner;
                     const buttonAgain = document.querySelector(
                         '.startagain',
                     ) as HTMLElement;
